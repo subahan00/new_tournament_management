2246da1651fb1bed99cbc7baf746209def2f298f
subahan00 <subahanmulla007@gmail.com>
2025-07-30 17:09:26 +0530

    Update Announcement.js

--- server/models/Announcement.js
+++ server/models/Announcement.js
@@ -9,9 +9,12 @@ const AnnouncementSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now,
+        // This tells MongoDB to automatically delete documents after 1 week (7 days)
+        // 60 seconds * 60 minutes * 24 hours * 7 days = 604800 seconds
+        expires: 604800, 
     },
 });
 
 const Announcement = mongoose.model('Announcement', AnnouncementSchema);
 
-module.exports = Announcement; // ✅ Add this line!
+module.exports = Announcement;
